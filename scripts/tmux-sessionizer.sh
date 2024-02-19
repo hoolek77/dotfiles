b028aa67e021914496798dd806211a7fc318b726
hoolek77 <szymon.kin@gmail.com>
2024-02-19 23:53:00 +0100

    Change scripts config

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,36 +1,16 @@
 #!/usr/bin/env bash
 
-path_to_strip=/Users/szymonkin/
 path_to_config=~/dev/personal/.dotfiles/scripts/scripts-config/tmux-sessionizer
-
-# join each line of config into a string separated by space
-
-find_paths=()
-
-# Read the file line by line, expand tilde, and append each line to the array
-while IFS= read -r path; do
-    # Check if the path starts with ~
-    if [[ $path == "~"* ]]; then
-        # Expand ~ to home directory
-        expanded_path="$HOME${path:1}"
-    else
-        expanded_path="$path"
-    fi
-    paths+=("$expanded_path")
-done < "$path_to_config"
-
 
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find "${paths[@]}" -mindepth 1 -maxdepth 1 -type d | sed "s|$path_to_strip||" | fzf)
+    selected=$(find ~/dev ~/dev/frontend/apps ~/dev/frontend/packages ~/dev/personal -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
   exit 0
 fi
-
-selected=$path_to_strip$selected
 
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
@@ -38,13 +18,11 @@ tmux_running=$(pgrep tmux)
 tmux_attached=$(tmux ls | grep attached | awk -F: '{print $1}')
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    echo "No tmux session running"
     tmux new-session -s $selected_name -c $selected
     exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    echo "Creating new session"
     tmux new-session -ds $selected_name -c $selected
 fi
 
