8357cf6470da6e53dbadaa7b28160798fd434996
hoolek77 <szymon.kin@gmail.com>
2024-06-11 23:30:25 +0200

    update plugins

--- nvim/lua/hoolek/plugins/wilder.lua
+++ nvim/lua/hoolek/plugins/wilder.lua
@@ -11,11 +11,11 @@ return {
     },
     config = function()
       local wilder = require("wilder")
-      local macchiato = require("catppuccin.palettes").get_palette("macchiato")
+      -- local macchiato = require("catppuccin.palettes").get_palette("macchiato")
 
       -- Create a highlight group for the popup menu
-      local text_highlight = wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground = macchiato.text } })
-      local mauve_highlight = wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = macchiato.mauve } })
+      -- local text_highlight = wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground = macchiato.text } })
+      -- local mauve_highlight = wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = macchiato.mauve } })
 
       -- Enable wilder when pressing :, / or ?
       wilder.setup({ modes = { ":", "/", "?" } })
@@ -36,11 +36,11 @@ return {
         "renderer",
         wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
           highlighter = wilder.basic_highlighter(),
-          highlights = {
-            default = text_highlight,
-            border = mauve_highlight,
-            accent = mauve_highlight,
-          },
+          -- highlights = {
+          -- default = text_highlight,
+          -- border = mauve_highlight,
+          -- accent = mauve_highlight,
+          -- },
           min_width = "100%",
           min_height = "40%",
           max_height = "40%",
