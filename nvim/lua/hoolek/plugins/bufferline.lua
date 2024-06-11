85bbada38e2700af1e482c649f2ff25ffd6a153c
hoolek77 <szymon.kin@gmail.com>
2024-06-11 23:29:06 +0200

    add bufferline

---
+++ nvim/lua/hoolek/plugins/bufferline.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          separator_style = "thin",
+          diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon .. count
+          end,
+        },
+      })
+    end,
+  },
+}
