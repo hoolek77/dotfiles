3c158e0c1ad36e6cfb227e301fb7417e06ab165d
hoolek77 <szymon.kin@gmail.com>
2024-08-01 21:19:15 +0200

    Update image

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dev ~/dev/frontend/apps ~/dev/frontend/packages ~/dev/personal -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/dev ~/dev/frontend/apps ~/dev/frontend/packages ~/dev/personal ~/dev/personal/.dotfiles -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
